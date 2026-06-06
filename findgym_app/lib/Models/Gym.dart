896114e95e88401394d441f316b51550473e94b6
fadhil Oktafian <Padilpadil84@mhs.mdp.ac.id>
2026-06-07 00:45:24 +0700

    update gym.dart

--- findgym_app/lib/Models/Gym.dart
+++ findgym_app/lib/Models/Gym.dart
@@ -1,31 +1,19 @@
 class Gym {
-  final String name;
-  final String location;
-  final String description;
-  final String built;
-  final String type;
-  final String? secondaryType;
-  final List<String> imageUrls;
-  final bool isFavorite;
-  final Map<String, String> schedule;
-  final String imageAsset;
-  final double rating;
-  final int ratingCount;
-
+  String id;
+  String nama;
+  String alamat;
+  String deskripsi;
+  String imageUrl;
+  double latitude;
+  double longitude;
 
   Gym({
-    required this.name,
-    required this.location,
-    required this.description,
-    required this.built,
-    required this.type,
-    required this.imageUrls,
-    this.secondaryType,
-    this.isFavorite = false,
-    this.rating = 0.0,
-    this.ratingCount = 0,
-    required this.schedule,
-    required this.imageAsset,
-    
+    required this.id,
+    required this.nama,
+    required this.alamat,
+    required this.deskripsi,
+    required this.imageUrl,
+    required this.latitude,
+    required this.longitude,
   });
 }
