ac0788c36c8524948f11bcdb023fe68489aa8d79
fadhil Oktafian <rifkifathurrahman1@mhs.mdp.ac.id>
2026-06-14 22:01:32 +0700

    update isi gym.dart

--- findgym_app/lib/Models/Gym.dart
+++ findgym_app/lib/Models/Gym.dart
@@ -1,19 +1,25 @@
 class Gym {
-  String id;
-  String nama;
-  String alamat;
-  String deskripsi;
-  String imageUrl;
-  double latitude;
-  double longitude;
+  final String id;
+  final String name;
+  final String location;
+  final String description;
+  final double latitude;
+  final double longitude;
+  final List<String> types;
+  final List<String> imageUrls;
+  final double rating;
+  final int ratingCount;
 
   Gym({
     required this.id,
-    required this.nama,
-    required this.alamat,
-    required this.deskripsi,
-    required this.imageUrl,
+    required this.name,
+    required this.location,
+    required this.description,
     required this.latitude,
     required this.longitude,
+    required this.types,
+    required this.imageUrls,
+    required this.rating,
+    required this.ratingCount,
   });
 }
